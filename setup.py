682b68f288b3f8f591a58c300e53842f4806e9e3
mishka251 <mishkabelka251@gmail.com>
2021-08-20 01:18:04 +0500

    попытка в setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname
+
+setup(
+    name='django-ovation-prime',
+    version='0.1.0',
+    install_requires=[
+        'Django >= 3.2.0',
+        'numpy >= 1.21.0',
+        'OvationPyme @ git+https://github.com/mishka251/OvationPyme.git@0.1.1#egg=ovationpyme',
+        'nasaomnireader @ git+https://github.com/mishka251/nasaomnireader.git@0.1.1#egg=nasaomnireader'
+    ],
+    dependency_links=[
+        'git+https://github.com/mishka251/OvationPyme.git@0.1.1#egg=ovationpyme',
+        'git+https://github.com/mishka251/nasaomnireader.git@0.1.1#egg=nasaomnireader'
+    ],
+    packages=find_packages(),
+    long_description=open(join(dirname(__file__), 'readme.md')).read(),
+)
