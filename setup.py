3ef8e422305e610786a8f66d417e49c10b155610
mishka251 <mishkabelka251@gmail.com>
2021-08-24 01:14:49 +0500

    setup.py

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ setup(
         'nasaomnireader @ git+https://github.com/mishka251/nasaomnireader.git@0.1.1#egg=nasaomnireader'
     ],
     dependency_links=[
-        'git+https://github.com/mishka251/OvationPyme.git@0.1.1#egg=ovationpyme',
-        'git+https://github.com/mishka251/nasaomnireader.git@0.1.1#egg=nasaomnireader'
+        # 'git+https://github.com/mishka251/OvationPyme.git@0.1.1#egg=ovationpyme',
+        # 'git+https://github.com/mishka251/nasaomnireader.git@0.1.1#egg=nasaomnireader'
     ],
     packages=find_packages(),
     long_description=open(join(dirname(__file__), 'readme.md')).read(),
